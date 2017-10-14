25d60a1ada5854ba4642d8cfe1fe76d3786fed87
bryanlee89 <minjae8989@gmail.com>
2017-10-14 15:10:11 -0700

    Stripe createCharge

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,5 +1,32 @@
+const keys = require("../config/keys");
+const stripe = require("stripe")(keys.stripeSecretKey);
+
 module.exports = app => {
-  app.post("/api/stripe", (req,res) => {
-    
-  })
+  app.post("/api/stripe", async (req, res) => {
+
+  const charge = await stripe.charges.create({
+      amount: 500,
+      currency: "usd",
+      source: req.body.id,
+      description: "$5 for 5 credits"
+    });
+
+    console.log(charge);
+  });
 };
+
+//
+//
+// var stripe = require("stripe")(
+//   "sk_test_C0jV1e3K2wG0VUXWgJVX3EbI"
+// );
+//
+// stripe.charges.create({
+//   amount: 2000,
+//   currency: "cad",
+//   source: "tok_visa", // obtained with Stripe.js
+//   description: "Charge for joshua.thompson@example.com"
+// }, function(err, charge) {
+//   // asynchronously called
+// });
+//
