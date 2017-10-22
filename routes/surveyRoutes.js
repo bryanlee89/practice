60a0005edb2b9c52bb6ff885d76802b13689d3be
bryanlee89 <minjae8989@gmail.com>
2017-10-22 08:48:59 -0700

    survey list backend frontend

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -45,6 +45,14 @@ module.exports = app => {
     res.send({});
   });
 
+  app.get("/api/surveys", requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false
+    });
+
+    res.send(surveys);
+  });
+
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
